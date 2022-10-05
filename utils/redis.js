8a274f4d670de245a2d159a71aa4f5c9e63e74a2
“olaholunuga” <olaholunuga@gmail.com>
2022-10-05 01:59:11 +0100

    create: redis connection

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    const client = createClient();
+    client.on(
+      'error',
+      (err) => console.log(err),
+    );
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = await promisify(this.client.get).bind(this.client);
+    const value = await asyncGet(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, {
+      EX: duration,
+    });
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
